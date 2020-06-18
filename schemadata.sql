cecb79b0634546cdd50e555ceb034b1f2bdfaef3
nehats05 <nehats1995@gmail.com>
2020-06-18 13:46:09 +0530

    adding schema file

---
+++ schemadata.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `projschema`.`flightsdata` (
+flightname varchar(255) primary key, flightdep varchar(255)
+);
+
+
+insert into flightsdata values ('Air Canada 8099','7:30 AM')
+insert into flightsdata values ('United Airline 6115','10:30 AM')
+insert into flightsdata values ('WestJet 6456','12:30 PM')
+insert into flightsdata values ('Delta 3833','03:00 PM')
