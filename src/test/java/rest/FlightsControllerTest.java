976a1ae40b1aef23560351877612ada1e3d7354c
nehats05 <nehats1995@gmail.com>
2020-07-10 20:33:55 +0530

    adding unit testing of controller and use case enhancement

---
+++ src/test/java/rest/FlightsControllerTest.java
@@ -0,0 +1,45 @@
+package rest;
+
+import com.flightSearch.app.Application;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = Application.class)
+@AutoConfigureMockMvc
+public class FlightsControllerTest extends AbstractTransactionalJUnit4SpringContextTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+   /*@Mock
+    private FlightsService flightsService;*/
+
+    /*@Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }*/
+
+    @Test
+    public void checkEmptyDetails() throws Exception{
+      mockMvc.perform(MockMvcRequestBuilders.get("/getFlights")
+              .param("duration","03:00 PM")
+                .accept(MediaType.APPLICATION_JSON))
+              .andDo(MockMvcResultHandlers.print())
+              .andExpect(MockMvcResultMatchers.status().isOk())
+              .andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.notNullValue()));
+    }
+
+}
